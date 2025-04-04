f317d2ecc3f09d0c385c67d2eb5bab3e23c9eb5e
reddii1 <81165693+reddii1@users.noreply.github.com>
2025-04-04 09:55:59 +0100

    Update bac8-render.py

--- ta-ansible-zftr-combined-deployments/bac8-render.py
+++ ta-ansible-zftr-combined-deployments/bac8-render.py
@@ -21,3 +21,33 @@ with open("gitlab_backup.ps1", "w") as file:
     file.write(result)
 
 print("Script generated as gitlab_backup.ps1!")
+
+
+
+
+
+#############################################
+
+from jinja2 import Template
+
+# Your test values
+data = {
+    "gitlab_base_url": "https://gitlab.com",
+    "gitlab_project_id": "43328313",
+    "gitlab_private_token": "glpat-xyxJz-ktxXkPjJzo3bAy",
+    "gitlab_branch": "test-case-data-dev",
+    "csv_directory": "F:\\Nectar\\CXA\\liferay-ce-portal-7.4.3.78-ga78\\test-case-data",
+    "version_tag": "2024.2.1",
+    "commit_message_format": "backup for {version} {date} [skip ci]"
+}
+
+# Load the template
+with open("gitlab_backup.ps1.j2", "r") as file:
+    template = Template(file.read())
+
+# Render it with your values
+result = template.render(data)
+with open("gitlab_backup.ps1", "w") as file:
+    file.write(result)
+
+print("Generated gitlab_backup.ps1!")
